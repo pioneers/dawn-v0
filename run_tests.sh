07435b77fa963dc9f0b43fe4239f392705c21c0d
Allen Li <allen@allen.li>
2015-02-27 16:06:09 -0800

    put single-run after

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,5 @@
 set -ev
 echo 2
-./node_modules/karma/bin/karma start --single-run --browsers NodeWebkit test/karma.conf.js
+./node_modules/karma/bin/karma start --browsers NodeWebkit test/karma.conf.js --single-run
 echo 4
 exit
