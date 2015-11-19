5bd1efa8e352a151e0a30c3550e5829864889eb9
bodiddles <vagrant@axiom>
2015-11-19 04:08:28 +0000

    Implemented Sensors

--- runtime/runtime.py
+++ runtime/runtime.py
@@ -5,6 +5,9 @@ import time
 import grizzly
 from api import Robot
 from api import Gamepads
+import hibike
+import deviceContext
+import time
 
 #Robot.init()
 
@@ -49,14 +52,21 @@ def p_watch(p):
     with pobslock:
         pobs.remove(p)
 
+student_ansible = Ansible('student_code')
+dawn_ansible = Ansible('dawn')
+runtime_ansible = Ansible('runtime')
+context = deviceContext.DeviceContext()
+h = hibike.Hibike(context)
+connectedDevices = h.getEnumeratedDevices()
+h.subToDevices(connectedDevices)
 
 while True:
-    command = ansible.recv() 
+    command = ansible.recv()
     if command:
         print("Message received from ansible!")
         msg_type, content = command['header']['msg_type'], command['content']
         if msg_type == 'execute':
-	    print("Ansible said to start the code")
+	        print("Ansible said to start the code")
             if not running_code:
                 p = subprocess.Popen(['python', 'student_code/student_code.py'])
                 with pobslock:
@@ -67,12 +77,33 @@ while True:
                 t.start()
                 running_code = True
         elif msg_type == 'stop':
-	    print("Ansible said to stop the code")
+	        print("Ansible said to stop the code")
             if running_code:
                 with pobslock:
                     print("killed")
                     for p in pobs: p.kill()
                 #kill all motor values
                 Robot.set_motor('motor0', 0)
-                Robot.set_motor('motor1', 0) 
+                Robot.set_motor('motor1', 0)
                 running_code = False
+	    runtime_ansible.sendMessage("sensor_value",connectedDevices)
+#        student_command = runtime_ansible.recv()
+#        if student_command:
+#            hibike_data = {}
+#            header = student_command['header']
+#            content = student_command['content']
+#            if header['msg_type'] == "get_sensor" and content:
+#                for name in content:
+#                        hibike_data{str(name)} = Robot.get_sensor(name)
+#                    except:
+##                    try:
+#                        print("sensor is not set")
+#            elif header['msg_type'] == 'set_motor' and content:
+##                runtime_ansible.send_message('sensor_data', hibike_data)
+#                for motor_name in content:
+#                    try:
+#                        Robot.set_motor(motor_name, content[motor_name])
+#                        print("motor is not set")
+#                    except:
+#                        #needs improvement
+#                runtime_ansible.send_message('set')
