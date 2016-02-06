f5d7072c75d376860962fdb62c66cb9f8f41440b
bodiddles <bdai87@gmail.com>
2016-02-06 13:19:03 -0800

    Implemented errors up to servos

--- runtime/api/Robot.py
+++ runtime/api/Robot.py
@@ -17,9 +17,11 @@ def get_motor(name):
     :returns: A value between -100 and 100, which is the power level of that motor.
     """
     name_to_value = mc.get('motor_values')
-    if name in name_to_value:
+    assert type(name) is str, "Type Mismatch: Must pass in a string"
+    try:
         return name_to_value[name]
-    return 'Error, motor with that name not found'
+    except KeyError:
+        raise KeyError("Motor name not found.")
 
 def set_motor(name, value):
     """Sets a motor to the specified power value.
@@ -27,21 +29,26 @@ def set_motor(name, value):
     :param name: A string that identifies the motor.
     :param value: A decimal value between -100 and 100, the power level you want to set.
     """
+    assert type(name) is str, "Type Mismatch: Must pass in a string to name."
+    assert type(value) is int or type(name) is float, "Type Mismatch: Must pass in an integer or float to value."
+    assert value <= 100 and value >= -100, "Motor value must be a decimal between -100 and 100 inclusive."
     name_to_value = mc.get('motor_values')
-    if name in name_to_value:
+    try:
         name_to_value[name] = value
         mc.set('motor_values', name_to_value)
-    else:
-        print("No motor with that name")
+    except KeyError:
+        raise KeyError("No motor with that name")
 
 # TODO: implement
 def get_sensor(name): #name is raw UID, with count added in front
     """Returns the value, or reading corresponding to the specified sensor.
     """
+    name = _lookup(name)
     all_data = mc.get('sensor_values')
-    if name in all_data:
+    try:
         return all_data[UUID]
-    return 'Error, sensor with that name not found'
+    except KeyError:
+        raise KeyError("No Sensor with that name")
 
 
 def get_all_motors():
@@ -61,6 +68,12 @@ def set_flag(name,light1,light2,light3,light4):  #TODO UID convert to int
     :param light3: An integer (0,1,2,3) which sets brightness for LED 3
     :param light4: An integer (0,1,2,3) which sets brightness for LED 4
     """
+    correct_range = range(4)
+    assert light1 in correct_range, "Error: input for light1 must be an integer between 0 and 3 inclusive"
+    assert light2 in correct_range, "Error: input for light2 must be an integer between 0 and 3 inclusive"
+    assert light3 in correct_range, "Error: input for light3 must be an integer between 0 and 3 inclusive"
+    assert light4 in correct_range, "Error: input for light4 must be an integer between 0 and 3 inclusive"
+    name = _lookup(name)
     flag_data = list(name) + list(light1) + list(light2) + list(light3) + list(light4)
     mc.set('flag_values',flag_data)
 
@@ -75,6 +88,9 @@ def set_LED(name,light,value): #TODO UID convert to int
     :param light: An integer (1,2,3,4) which identifies which LED top set.
     :param value: An integer (0,1,2,3) which sets brightness for the specified LED
     """
+    name = _lookup(name)
+    assert light in range(1,5), "Error: light number must be an Integer between 1 and 4 inclusive"
+    assert value in range(4),"Error: value must be an integer between 0 and 3 inclusive"
     flag_data = list(name) + [-1,-1,-1,-1]
     flag_data[light] = value
     mc.set('flag_values',flag_data)
@@ -90,6 +106,12 @@ def set_all_servos(name,servo0,servo1,servo2,servo3): #TODO How does the servos 
     :param servo2: An integer between 0 and 180 which sets the amount to (turn in degrees) for servo 2
     :param servo3: An integer between 0 and 180 which sets the amount to (turn in degrees) for servo 3
     """
+    name = _lookup(name)
+    correct_range = range(181)
+    assert servo0 in correct_range, "servo0 must be between 0 and 180 inclusive"
+    assert servo1 in correct_range, "servo1 must be between 0 and 180 inclusive"
+    assert servo2 in correct_range, "servo2 must be between 0 and 180 inclusive"
+    assert servo3 in correct_range, "servo3 must be between 0 and 180 inclusive"
     servo_data = list(name) + list(servo0) + list(servo1) + list(servo2) + list(servo3)
     mc.set('servo_values',servo_data)
 
@@ -102,7 +124,14 @@ def set_servo(name,servo,value):
     :param servo: A integer (0,1,2,3) which identifies which servo to turn
     :param value: An integer between 0 and 180 which sets the amount to turn (in degrees)
     """
+    name = _lookup(name)
     servo_data = list(name) + [-1,-1,-1,-1]
     servo_data[servo + 1] = value
     mc.set('servo_values',servo_data)
+
+
+
+
+class SensorValueOutOfBounds(Exception):
+    pass
 
