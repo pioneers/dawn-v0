643abb1349b5fa8e0422813ec7dc74b3ebbdf2e8
karthik-shanmugam <karthik@karthiks.me>
2016-04-05 16:49:44 -0700

    add driving to teleop sample

--- dawn/js/actions/FieldActions.js
+++ dawn/js/actions/FieldActions.js
@@ -31,7 +31,29 @@ var FieldActions = {
         });
       } else {
         Ansible.sendMessage('execute', {
-          code: "import time\nprint time.strftime('starting teleop at %l:%M:%S %p')\n"
+          code: `
+"""
+Sample student code that drives the robot based off of joystick values.
+Copyright 2016. Pioneers in Engineering.
+"""
+from api.Gamepads import *
+from api.Robot import *
+import time
+print time.strftime('starting autonomous at %l:%M:%S %p')
+while True:
+    #Change to correct gamepad_index if necessary
+    gamepad_index = 0
+
+    #Get the joystick values of the left y-axis and right y-axis
+    joystick_1_value = get_axis(gamepad_index, Joystick.LEFT_Y)
+    joystick_2_value = get_axis(gamepad_index, Joystick.RIGHT_Y)
+
+    #Drives the robot at 75% of its poewr, based off of the joystick values.
+    set_motor('motor0', 0.75 * joystick_1_value)
+    set_motor('motor1', 0.75 * joystick_2_value)
+
+    time.sleep(.05)
+`
         });       
       }
       console.log("RUNNING SOBOT: " + (msg.autonomous ? "AUTONOMOUS" : "TELEOP"))
